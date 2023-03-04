50177a56d7eedb000ed3a4212edafa3b5d6d6d7e
Ojonuba Jeremiah <ojonuba93@gmail.com>
2023-03-05 00:01:27 +0100

    config file for api

---
+++ api/config/config.py
@@ -0,0 +1,33 @@
+import os
+from datetime import timedelta
+
+from decouple import config
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+
+
+class Config:
+    SECRET_KEY = config("SECRET_KEY", "secret")
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=120)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(hours=72)
+    JWT_SECRET_KEY = config('JWT_SECRET_KEY')
+
+
+class DevConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "db.sqlite3")
+
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_DATABASE_URI = "sqlite://"
+
+class ProdConfig(Config):
+    pass
+
+
+config_dict = {"dev": DevConfig, "prod": ProdConfig, "test": TestConfig}
