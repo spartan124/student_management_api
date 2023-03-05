e3a82828ea3676286ab8fe21c9d9f9485d2f0236
Ojonuba Jeremiah <ojonuba93@gmail.com>
2023-03-05 16:27:11 +0100

    added authorizations to api and JWTManager

--- api/__init__.py
+++ api/__init__.py
@@ -3,7 +3,9 @@ from flask_restx import Api
 from .db import db
 from .config.config import config_dict
 from .auth.views import namespace as auth_namespace
-from .models.students import Students
+from .models.students import Student
+from flask_jwt_extended import JWTManager
+
 
 def create_app(config=config_dict['dev']):
     app = Flask(__name__)
@@ -12,7 +14,22 @@ def create_app(config=config_dict['dev']):
     
     db.init_app(app)
     
-    api = Api(app)
+    jwt = JWTManager(app)
+    authorizations= {
+        "Bearer Auth": {
+            'type': 'apiKey',
+            'in':'header',
+            'name':'Authorization',
+            'description': 'Add a JWT token to the header with ** Bearer &lt;JWT&gt; token to authorize **'
+        }
+    }
+    
+    api = Api(app,
+              title='School Management API',
+              description='A basic school management REST API service',
+              authorizations=authorizations,
+              security='Bearer Auth'
+            )
     
     
     
@@ -22,7 +39,7 @@ def create_app(config=config_dict['dev']):
     def make_shell_context():
         return {
             'db': db,
-            'Students': Students,
+            'Student': Student,
             # 'Order': Order
 
         }
