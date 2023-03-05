e43b99928dc26eed0768ed3a5a009efdd34ada8a
Ojonuba Jeremiah <ojonuba93@gmail.com>
2023-03-05 02:08:39 +0100

    registered the auth_namespace and flask shell

--- api/__init__.py
+++ api/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_restx import Api
 from .db import db
 from .config.config import config_dict
+from .auth.views import namespace as auth_namespace
+from .models.students import Students
 
 def create_app(config=config_dict['dev']):
     app = Flask(__name__)
@@ -14,5 +16,14 @@ def create_app(config=config_dict['dev']):
     
     
     
+    api.add_namespace(auth_namespace)
     
+    @app.shell_context_processor
+    def make_shell_context():
+        return {
+            'db': db,
+            'Students': Students,
+            # 'Order': Order
+
+        }
     return app
