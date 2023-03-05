b70eaf030004d1c5d6dbb44a5a0da4769c11813c
Ojonuba Jeremiah <ojonuba93@gmail.com>
2023-03-05 02:06:40 +0100

    added the student model

---
+++ api/models/students.py
@@ -0,0 +1,26 @@
+from ..db import db
+
+class Students(db.Model):
+    __tablename__ = 'students'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), nullable=False, unique=True)
+    password_hash = db.Column(db.Text(), nullable=False)
+
+    def __repr__(self):
+        return f"<Student {self.name}>"
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+        
+    def update(self):
+        db.session.commit()
+        
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+        
+    @classmethod
+    def get_by_id(cls, id):
+        return cls.query.get_or_404(id)
