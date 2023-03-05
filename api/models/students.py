88d2ed7f8c2392fd5ff6e34c9660ae4b2b7061d1
Ojonuba Jeremiah <ojonuba93@gmail.com>
2023-03-05 16:29:25 +0100

    slight mod to students model

--- api/models/students.py
+++ api/models/students.py
@@ -1,6 +1,6 @@
 from ..db import db
 
-class Students(db.Model):
+class Student(db.Model):
     __tablename__ = 'students'
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(80), nullable=False)
