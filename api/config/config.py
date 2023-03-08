e95dc0e278353307f5240b4b8f0d7b48fdadb070
Ojonuba Jeremiah <ojonuba93@gmail.com>
2023-03-08 21:39:59 +0100

    updated config file to add ProdConfig

--- api/config/config.py
+++ api/config/config.py
@@ -27,7 +27,10 @@ class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = "sqlite://"
 
 class ProdConfig(Config):
-    pass
+    DEBUG = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_DATABASE_URI = 'postgres://rityixbcuhbgay:c494102735987ed7b7192dace5b26e383cc8a2e269db794b8f767d6bc1db0d68@ec2-34-192-210-139.compute-1.amazonaws.com:5432/d6g39g5uha01e2'
 
 
 config_dict = {"dev": DevConfig, "prod": ProdConfig, "test": TestConfig}
