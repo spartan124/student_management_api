19047da3df96ee067eed1b70ce169e5d545819ac
Ojonuba Jeremiah <ojonuba93@gmail.com>
2023-03-15 13:38:05 +0100

    minor changes to fix issues

--- api/models/courses.py
+++ api/models/courses.py
@@ -10,10 +10,11 @@ class Course(db.Model):
     description = db.Column(db.String(500), nullable=True)
     credit_unit = db.Column(db.Integer(), nullable=False)
     teacher_id = db.Column(db.Integer(), db.ForeignKey('teachers.teacher_id'))
-    students = db.relationship('Student', secondary='student_course')
+    students = db.relationship('StudentCourse', backref='courses')
+    # student_course = db.relationship('StudentCourse', backref='courses')
     
     def __repr__(self):
-        return f"Course Code: {self.course_code}"
+        return f"{self.course_code}"
     
     def save(self):
         db.session.add(self)
--- api/models/student_course.py
+++ api/models/student_course.py
@@ -3,12 +3,14 @@ from ..db import db
 class StudentCourse(db.Model):
     __tablename__= 'student_course'
     student_id = db.Column(db.Integer(), db.ForeignKey('students.student_id'), primary_key=True)
-    course_id = db.Column(db.Integer(), db.ForeignKey('courses.course_id'), primary_key=True)
-    grade = db.Column(db.String())
-    # students = db.relationship('Student', backref='student_course')
-    #credit_unit = db.Column(db.Float(), nullable=False)
-    #earned_credit = db.Column(db.Float(), nullable=True)
-    #teacher = db.relationship('Teacher', secondary='courses', backref='student_courses')
+    course_id = db.Column(db.Integer(), db.ForeignKey('courses.course_id'))
+    grade = db.Column(db.String(1))
+    earned_credit = db.Column(db.Float())
+    gpa = db.Column(db.Float())
+    
+    #students = db.relationship('Student', back_populates='courses')
+    course = db.relationship('Course', backref='student_course')
+   
     
     def __repr__(self):
         return f"<Student Course ID {self.student_id}>"
@@ -27,3 +29,15 @@ class StudentCourse(db.Model):
     @classmethod
     def get_by_id(cls, id):
         return cls.query.get_or_404(id)
+    
+    @property
+    def earned_credit(self):
+        if self.grade == 'A':
+            return self.course.credit_unit * 5
+        elif self.grade == 'B':
+            return self.course.credit_unit * 4
+        elif self.grade == 'C':
+            return self.course.credit_unit * 3
+        else:
+            return 0
+        
