1c3756f5b846467ac7f47265e6fa2b76ce556e69
spartan124 <52575785+spartan124@users.noreply.github.com>
2023-03-18 18:47:16 +0000

    pytest for courses endpoint

---
+++ api/test/test_courses.py
@@ -0,0 +1,163 @@
+import unittest
+import json
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_jwt_extended import create_access_token, create_refresh_token
+from .. import create_app
+from ..config.config import config_dict
+from ..db import db
+from ..models import Student, Course, StudentCourse
+from ..resources.courses import clone_course_model
+
+
+class CoursesTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app(config=config_dict["test"])
+        self.appctx = self.app.app_context()
+        self.appctx.push()
+        self.client = self.app.test_client()
+        db.create_all()
+
+    def tearDown(self):
+        db.drop_all()
+        self.appctx.pop()
+        self.app = None
+        self.client = None
+        
+    def test_add_course(self):
+        
+        #Generate access_token
+        token = create_access_token(identity='teststudent@test.io')
+        headers = {
+            'Authorization': f'Bearer {token}'
+        }
+        #Test adding a course
+        course_data = {
+            "course_title": "Test Course",
+            "course_code": "TEST101",
+            "description": "This is a test course",
+            "credit_unit": 3,
+            "teacher_id": 1
+        }
+        response = self.client.post('/course/', json=course_data, headers=headers)
+        self.assertEqual(response.status_code, 201)
+
+        # Test adding a course with same course code again
+        response = self.client.post('/course/', json=course_data, headers=headers)
+        self.assertEqual(response.status_code, 403)
+        
+        #Test get all courses
+        response = self.client.get('/course/', headers=headers)
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(data, list)
+        self.assertGreater(len(data), 0)
+        
+    def test_get_courses(self):
+        token = create_access_token(identity='teststudent@test.io')
+        headers = {
+            'Authorization': f'Bearer {token}'
+        }
+        course = Course(
+            course_title="Test Course",
+            course_code="TC",
+            description="A test course",
+            credit_unit=3,
+            teacher_id=1,
+        )
+        course.save()
+        response = self.client.get("/course/{}".format(course.course_id), headers=headers)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json['course_id'], course.course_id)
+        self.assertEqual(response.json['course_code'], course.course_code)
+        
+        
+        #Test course not found
+        response = self.client.get("/course/99", headers=headers)
+        self.assertEqual(response.status_code, 404)
+
+    def test_update_course(self):
+        token = create_access_token(identity='teststudent@test.io')
+        headers = {
+            'Authorization': f'Bearer {token}'
+        }
+        course = Course(
+            course_title="Test Course",
+            course_code="TEST101",
+            description="This is a test course",
+            credit_unit=3,
+            teacher_id=1,
+        )
+        course.save()
+
+        data = {
+            "course_title": "Updated Test Course",
+            "course_code": "TEST102",
+            "description": "This is an updated test course",
+            "credit_unit": 4,
+            "teacher_id": 2,
+        }
+        response = self.client.put("/course/{}".format(course.course_id), headers=headers, json=data)
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.content_type, "application/json")
+        
+        updated_course = Course.query.filter_by(course_id=course.course_id).first()
+        
+        self.assertEqual(updated_course.course_title, data["course_title"])
+        self.assertEqual(updated_course.course_code, data["course_code"])
+        self.assertEqual(updated_course.description, data["description"])
+        self.assertEqual(updated_course.credit_unit, data["credit_unit"])
+        self.assertEqual(updated_course.teacher_id, data["teacher_id"])
+    
+    def test_delete_a_course(self):
+        token = create_access_token(identity='teststudent@test.io')
+        headers = {
+            'Authorization': f'Bearer {token}'
+        }
+        course = Course(
+            course_title="Mathematics",
+            course_code="MAT101",
+            description="An introductory math course",
+            credit_unit=3,
+            teacher_id=1,
+        )
+        course.save()
+            
+        response = self.client.delete("/course/{}".format(course.course_id), headers=headers)
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.data)
+        self.assertEqual(data["message"], "Course successfully deleted")
+
+    def test_enrolled_students(self):
+        token = create_access_token(identity='teststudent@test.io')
+        headers = {
+            'Authorization': f'Bearer {token}'
+        }
+        
+        course = Course(
+        course_title="Test Course",
+        course_code="TST101",
+        description="This is a test course",
+        credit_unit=3,
+        teacher_id=1,
+        )
+        course.save()
+        
+        student1 = Student(name='Test Student',
+                           email='teststudent@test.com',
+                           password_hash='password'
+                           )
+        student1.save()
+        student2 = Student(name='Fun Student',
+                           email='funstudent@test.com',
+                           password_hash='password'
+                           )
+        student2.save()
+        
+        course.students.append(student1)
+        course.students.append(student2)
+
+        response = self.client.get("/course/{}/students".format(course.course_id), headers=headers)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json), 2)
