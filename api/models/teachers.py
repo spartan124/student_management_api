9aca0e77084aacce7da9499711efe2c75642babe
Ojonuba Jeremiah <ojonuba93@gmail.com>
2023-03-22 00:28:29 +0100

    added pass hash column, removed some db methods

--- api/models/teachers.py
+++ api/models/teachers.py
@@ -7,20 +7,21 @@ class Teacher(db.Model):
     name = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(80), nullable=False, unique=True)
     role = db.Column(db.String(), nullable=False)
+    password_hash = db.Column(db.Text(), nullable=False)
     courses = db.relationship('Course', backref='student_courses')
     def __repr__(self):
         return f"<Teacher {self.name}>"
     
-    def save(self):
-        db.session.add(self)
-        db.session.commit()
+    # def save(self):
+    #     db.session.add(self)
+    #     db.session.commit()
         
-    def update(self):
-        db.session.commit()
+    # def update(self):
+    #     db.session.commit()
         
-    def delete(self):
-        db.session.delete(self)
-        db.session.commit()
+    # def delete(self):
+    #     db.session.delete(self)
+    #     db.session.commit()
         
     @classmethod
     def get_by_id(cls, id):
