c53d1ba6c52b06765b21e2398e5555b2addecd39
Ojonuba Jeremiah <ojonuba93@gmail.com>
2023-03-22 23:27:47 +0100

    created fns to aid easy creating of user & course

--- api/test/__init__.py
+++ api/test/__init__.py
@@ -1,5 +1,5 @@
 from ..models import Admin, Teacher, Student, Course, StudentCourse, StudentResult
-
+from werkzeug.security import generate_password_hash
 
 def create_course(course_title="Test Course", course_code="TST101", description="This is a test course", credit_unit=3, teacher_id=1):
     course = Course(
@@ -11,22 +11,22 @@ def create_course(course_title="Test Course", course_code="TST101", description=
     )
     return course
 
-def create_student():
+def create_student(name='Test Student', email='teststudent@test.com', password_hash=generate_password_hash('password'), role='student'):
     student = Student(
-        name='Test Student',
-        email='teststudent@test.com',
-        password_hash='password',
-        role='student'
+        name=name,
+        email=email,
+        password_hash=password_hash,
+        role=role
     )
     return student
 
 
-def create_teacher():
+def create_teacher(name='Test Teacher', email='testteacher@test.com', password_hash=generate_password_hash('password'), role='teacher'):
     teacher = Teacher(
-        name='Test Teacher',
-        email='testteacher@test.com',
-        password_hash='password',
-        role='teacher'
+        name=name,
+        email=email,
+        password_hash=password_hash,
+        role=role
     )
     return teacher
 
